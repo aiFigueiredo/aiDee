6a874712b2132083a3fcdef8d921cedcd5637257
Jose Miguel Figueiredo <JoseMiguel.Figueiredo@synchronoss.com>
2020-02-23 23:13:35 +0000

    Add aiDee Project to Workspace

---
+++ aiDee/aiDee/aiDee.h
@@ -0,0 +1,19 @@
+//
+//  aiDee.h
+//  aiDee
+//
+//  Created by Jose Miguel Figueiredo on 22/02/2020.
+//  Copyright © 2020 Jose Miguel Figueiredo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for aiDee.
+FOUNDATION_EXPORT double aiDeeVersionNumber;
+
+//! Project version string for aiDee.
+FOUNDATION_EXPORT const unsigned char aiDeeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aiDee/PublicHeader.h>
+
+
